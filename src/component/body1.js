8e40d005a967325217df252dadac61d55bec748a
Kanimozhi901/ <kanisabari555@gmail.com>
2023-11-01 08:29:54 +0530

    first commit

---
+++ src/component/body1.js
@@ -0,0 +1,35 @@
+import React,{useState} from 'react'
+function body1(){
+    const[counts1,setCounts1]=useState(100);
+    const[counts2,setCounts2]=useState(0);
+    
+    function myfunc1()
+    {
+        setCounts1(counts1+1)
+    }
+    function myfunc2()
+    {
+        setCounts1(counts1-1)
+    }
+    function myfunc3()
+    {
+        setCounts2(counts2+1)
+    }
+    
+
+
+
+  return (
+    <div>
+    
+    <button onClick={myfunc1}>Increase</button>
+    <button onClick={myfunc2}>Decrease</button>
+    <span>{counts1}</span>
+    <button onMouseOver={myfunc3}>{counts1}</button>
+    <span>{counts2}</span>
+    </div>
+  )
+  }
+
+export default body1
+
